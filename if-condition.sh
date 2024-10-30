14f96741d0bc9f1499e394ea98fc9b13adfe7e7b
sravandvs <sravandvs6@gmail.com>
2024-10-30 15:09:01 +0530

    inputs

---
+++ if-condition.sh
@@ -0,0 +1,36 @@
+#If found in 3 forms
+#Simple If
+
+#if [ expression ]; then
+#commands
+#fi
+
+#If Else
+
+#if[expression]; then
+#  Commands-T
+#else
+#  Commands-If
+#fi
+
+#Else if
+
+#if [expression-a]; then
+#  commands-a
+#elif [expression-b]; then
+#  commands-b
+#else
+#  commands
+#fi
+
+#Expressions
+#Strings
+#[abc == bac]
+#[$a == $x]
+#Operators : ==, !=, -z
+
+#Numbers
+#[1 -eq 2]
+#-eq, -ne, -gt, -ge, -le, -le
+
+#Files
